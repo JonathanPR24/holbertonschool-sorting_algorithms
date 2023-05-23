e0b875a75e36cc2084d186bcd376500d5c889860
luisayala323 <luisayala323@gmail.com>
2023-05-23 22:20:27 +0000

    selection sort

--- sort.h
+++ sort.h
@@ -1,6 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stdio.h>
 #include <stdlib.h> /* Include the standard library for the size_t type */
 
 /**
@@ -24,5 +25,6 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 
 #endif /* End of header guard */
