c98f11e4b1be85b14d0ea75802d4870207381a8d
JonathanPR24 <jonathanperez604@gmail.com>
2023-05-26 01:46:00 +0000

    function that searches for a value in an array of integers

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,31 +2,35 @@
 
 /**
  * bubble_sort - sorts an array of integers in ascending order
- * @array: pointer to the first element of the array
- * @size: number of elements in the array
- *
- * Description: This function sorts the given array in ascending order
- * using the bubble sort algorithm.
- *
- * Return: void
- **/
+ * @array: the array to be sorted.
+ * @size: number of elements in @array.
+ */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int temp;
+	/*Variable declarations*/
+	size_t i, j;   /* Loop counters. */
+	int tmp;/*Temporary variable for swapping elements*/
 
-	if (!array || size < 2)
+	/*If the array is NULL or has fewer than 2 elements, no sorting is needed*/
+	if (array == NULL || size < 2)
 		return;
 
+	/*i loops from 0 to size - 2*/
 	for (i = 0; i < size - 1; i++)
 	{
+		/*j loops from 0 to size - i - 2*/
 		for (j = 0; j < size - i - 1; j++)
 		{
+			/*If the current element is greater than the next one, swap them*/
 			if (array[j] > array[j + 1])
 			{
-				temp = array[j];
+
+				/*Swapping array[j] and array[j+1] using a temporary variable tmp*/
+				tmp = array[j];
 				array[j] = array[j + 1];
-				array[j + 1] = temp;
+				array[j + 1] = tmp;
+
+				/*Printing the array after swapping elements*/
 				print_array(array, size);
 			}
 		}
